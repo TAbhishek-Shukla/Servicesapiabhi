305c8ac0b2101cf9fdbc9b065d459081ecd3e570
TAbhishek-Shukla <abhishukla99111@gmail.com>
2024-03-31 12:47:04 +0530

    fourth try

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express= require('express');
 const app= express();
+const cors = require('cors');
+
 const PORT =  process.env.PORT   ;
 // http://localhost:4500/api/products
 const products_routes= require('./routes/products');
 const connectDB = require('./db/connect');
-
+const corsOptions={
+        origin: '*',
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 app.use('/api/products',products_routes);
 
 
